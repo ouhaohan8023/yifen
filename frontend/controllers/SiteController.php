453a43da0d4eb69a95a1ce9169b9f3b83e2de66b
ohh <mrmercury@foxmail.com>
2017-06-07 14:43:20 +0800

    1.0.6

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -119,6 +119,10 @@ class SiteController extends Controller
 //            return $oauth->redirect();
             // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
              $oauth->redirect()->send();
+        }else{
+            $user = $oauth->user();
+            var_dump($user->getOriginal());
+            die;
         }
 
         return $this->render('index');
