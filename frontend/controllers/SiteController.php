0a517638829edfb241c9254b4e24761a66c27b0b
ohh <mrmercury@foxmail.com>
2017-06-07 15:32:16 +0800

    1.0.12

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -113,7 +113,6 @@ class SiteController extends Controller
         ];
         $app = new Application($options);
         $oauth = $app->oauth;
-        var_dump($oauth->user());die;
         // 未登录
         if (empty($_SESSION['wechat_user'])) {
             $_SESSION['target_url'] = 'user/profile';
@@ -130,8 +129,43 @@ class SiteController extends Controller
     }
 
     public function actionIn(){
-        var_dump($_GET['code']);
+        //配置参数
+        $config = [
+          'debug'     => true,
+          'app_id'  => 'wx99c791ac5995b5e9',         // AppID
+          'secret'  => '3c1c7fad8cd8923bc037994762eff667',     // AppSecret
+          'token'   => 'ohhcms',          // Token
+          'aes_key' => '',                    // EncodingAESKey，安全模式下请一定要填写！！！
+          'log' => [
+            'level'      => 'debug',
+            'permission' => 0777,
+            'file'       => '/tmp/easywechat.log',
+          ],
+        ];
+
+        $app = new Application($config);
+        $oauth = $app->oauth;
+// 获取 OAuth 授权结果用户信息
+        $user = $oauth->user();
+        $user = $user->toArray();
+        var_dump($user);
         die;
+//获得openid
+        $openid = $user['id'];
+//通过openid找到user在joomla系统内的id
+//这里应该有一张数据表用来记录joomla内用户的id和openid之间的一一对应关系
+//当用户第一次使用微信授权登录时，应该提示用户注册或者绑定已有账号，把用户的openid和用户的在joomla的id写入上述的表中
+//当用户第二次通过微信授权登录时，直接通过openid在这张表里查询到用户的id，然后生成joomla的会话
+//这里把过程省略..假设得出结果user在系统内的id为270（我joomla的管理员id）
+        $uid =270;
+        $instance = JUser::getInstance();
+        $instance->load($uid);
+        $session = JFactory::getSession();
+//生成会话
+        $session->set('user', $instance);
+        $targetUrl = 'http://【域名】/joomla/index.php';
+// 跳转到joomla首页
+        header('location:'. $targetUrl);
 
     }
 //    新用户入口
