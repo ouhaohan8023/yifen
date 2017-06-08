c62d95b2aa01b6e444d20bda7d761398c0ec3f4a
ohh <mrmercury@foxmail.com>
2017-06-08 12:53:58 +0800

    1.0.41

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -129,10 +129,10 @@ class SiteController extends Controller
                 return $this->goBack();
             }
         }else{
-            $array = array([["SignupForm"]=>[
-              ["username"]  => 'o-UzxvtTllA4xZZqHdOviwZIajPQ',
-              ["email"]  => 'ohh@ohhcms.com',
-              ['password'] => '123456',
+            $array = array(["SignupForm"=>[
+              "username"  => 'o-UzxvtTllA4xZZqHdOviwZIajPQ',
+              "email"  => 'ohh@ohhcms.com',
+              'password' => '123456',
             ]]);
             var_dump($array);die;
 
