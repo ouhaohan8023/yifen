dde1fb266fde79df4661e9365615d28dbeb0a658
ohh <mrmercury@foxmail.com>
2017-06-08 16:03:48 +0800

    1.0.81

--- frontend/controllers/YiUserController.php
+++ frontend/controllers/YiUserController.php
@@ -68,7 +68,8 @@ class YiUserController extends Controller
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['site/home', 'id' => $model->u_openid]);
         } else {
-            var_dump(Yii::$app->user->isGuest);die;
+//            var_dump(Yii::$app->user->isGuest);die;
+            var_dump(Yii::$app->user->identity->username);die;
             $model->u_openid = Yii::$app->user->identity->username;
             $model->u_wx_name = YiUser::find()->where(['u_openid'=>$model->u_openid])->select('u_wx_name')->one();
             return $this->render('create', [
