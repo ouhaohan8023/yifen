476119fe1ea29dba01c43bcde34c82ca8cb45de8
ohh <mrmercury@foxmail.com>
2017-06-08 15:25:25 +0800

    1.0.73

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -283,17 +283,12 @@ $this->title = 'My Yii Application';
     <!-- Large modal -->
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
 
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title" id="myLargeModalLabel">Large modal</h4>
-            </div>
-            <div class="modal-body">
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
                 ...
             </div>
-        </div><!-- /.modal-content -->
+        </div>
     </div>
 
 <?php
