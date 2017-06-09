958e7f46612c555490cff36003370a81bf690333
ohh <mrmercury@foxmail.com>
2017-06-09 11:23:58 +0800

    1.0.47

--- frontend/views/yi-shop/index.php
+++ frontend/views/yi-shop/index.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="yi-shop-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 style="text-align: center"><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
